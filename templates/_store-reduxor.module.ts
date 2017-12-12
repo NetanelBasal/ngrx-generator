93bd84c3fa71f320ba680fdb7852460e92a676e3
kmathy <kevin.mathy95@gmail.com>
2017-12-12 13:18:25 +0100

    Update _store-reduxor.module.ts
    Add forgotten HttpClientModule

--- templates/_store-reduxor.module.ts
+++ templates/_store-reduxor.module.ts
@@ -3,11 +3,13 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { reducers, metaReducers } from './app.store';
 import { AllEffects } from './all-effects';
+import { HttpClientModule } from '@angular/common/http';
 // -- IMPORT SERVICES --
 import { {{ properCase name }}Service } from './{{ folder name "services" }}/{{ kebabCase name }}.service';
 
 @NgModule({
     imports: [
+        HttpClientModule,
         StoreModule.forRoot(reducers, { metaReducers }),
         EffectsModule.forRoot([...AllEffects]),
     ],
