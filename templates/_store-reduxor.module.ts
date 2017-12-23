ea19304d8cc3566c3a7dea5e2014a15d0deff745
Kevin MATHY <kmathy@adneom.com>
2017-12-23 20:41:41 +0100

    store-devtools

--- templates/_store-reduxor.module.ts
+++ templates/_store-reduxor.module.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+    import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { reducers, metaReducers } from './app.store';
 import { AllEffects } from './all-effects';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,6 +13,9 @@ import { {{ properCase name }}Service } from './{{ folder name "services" }}/{{ 
         HttpClientModule,
         StoreModule.forRoot(reducers, { metaReducers }),
         EffectsModule.forRoot([...AllEffects]),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25, //  Retains last 25 states
+        })
     ],
     exports: [],
     providers: [
